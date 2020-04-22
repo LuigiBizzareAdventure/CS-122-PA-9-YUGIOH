9a3eaff6121659d6a7675b95c58cef7a01cdfbf1
Up-Bound <61359739+Up-Bound@users.noreply.github.com>
2020-04-21 18:35:44 -0700

    Add files via upload

---
+++ Game.h
@@ -0,0 +1,37 @@
+#include "Header.h"
+#include "Player.h"
+class Game {
+private:
+	int round;
+	Player player[2];
+public:
+	Game(List preDeck0, List preDeck1) {
+		int count = 0;
+		Card temp;
+		while (count != DECK_SIZE) {
+			preDeck0.card(count, temp);
+			player[0].insertCardtoDeck(temp);
+			preDeck1.card(count, temp);
+			player[1].insertCardtoDeck(temp);
+			count++;
+		}
+		round = 1;
+		player[0].setLife();
+		player[1].setLife();
+	}
+	void Start();
+	int winnerCheck();//returns a 0 if no winner, 1 if player 1 wins, 2 if player 2 wins, or 3 for draw.
+	void swapTurn(int&);
+	void compSetPhase();
+	void playerSetPhase();
+
+	void compBattlePhase();
+	void playerBattlePhase();
+
+	void damageCalculation();
+
+
+
+
+	void display();
+};
---
+++ List.cpp
@@ -0,0 +1,120 @@
+#include "List.h"
+
+void List::addCard(Card newCard) {
+	ListNode* x = new ListNode;
+	x->card = newCard;
+	x->next = nullptr;
+	ListNode* current = top;
+	ListNode* prev = nullptr;
+	if (top == nullptr) {
+		top = x;
+		bottom = x;
+	}
+	else {
+		prev = current;
+		current = current->next;
+		while (current != nullptr) {
+			prev = current;
+			current = current->next;
+		}
+		prev->next = x;
+		bottom = x;
+
+	}
+}
+
+int List::size(void) {
+	int count = 0;
+	ListNode* current = top;
+	ListNode* prev;
+	if (current == nullptr) {
+		count = 0;
+	}
+	else {
+		while (current != nullptr) {
+			count++;
+			prev = current;
+			current = current->next;
+		}
+	}
+	return count;
+}
+
+bool List::card(int x, Card& card) {
+	int count = 0;
+	ListNode* current = top;
+	while (current != nullptr && count != x) {
+		count++;
+		current = current->next;
+	}
+	if (current == nullptr) {
+		cout << "You tried accessing a card that is out of bounds." << endl;
+		EnterKey();
+		return false;
+	}
+	else {
+		card = current->card;
+		return true;
+	}
+}
+
+bool List::removeCard(int x, Card& store) {
+	int count = 0;
+	ListNode* current = top;
+	ListNode* prev = nullptr;
+	ListNode* temp = nullptr;
+	while (current != nullptr && count != x) {
+		count++;
+		prev = current;
+		current = current->next;
+	}
+	if (current == nullptr) {
+		cout << "There is no card in postition " << x << " to delete." << endl;
+		EnterKey();
+		return false;
+	}
+	else if (prev != nullptr && current != nullptr) {
+		prev->next = current->next;
+		store = current->card;
+		delete current;
+		return true;
+	}
+	else if (prev == nullptr && current->next != nullptr) {
+		store = current->card;
+		temp = current->next;
+		delete current;
+		top = temp;
+	}
+	else {
+		store = current->card;
+		delete current;
+		top = nullptr;
+		return true;
+	}
+}
+
+bool List::removeCard(int x) {
+	int count = 0;
+	ListNode* current = top;
+	ListNode* prev = nullptr;
+	while (current != nullptr && count != x) {
+		count++;
+		prev = current;
+		current = current->next;
+	}
+	if (current == nullptr) {
+		cout << "There is no card in postition " << x << " to delete." << endl;
+		EnterKey();
+		return false;
+	}
+	else if (prev != nullptr) {
+		prev->next = nullptr;
+		delete current;
+		return true;
+	}
+	else {
+			delete current;
+			top = nullptr;
+		return true;
+	}
+}
